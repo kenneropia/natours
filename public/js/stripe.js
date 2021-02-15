cb87974e99ae333ea672cf3461f884e85839f78a
kenneropia <adetunmbikenny@gmail.com>
2021-02-15 11:24:33 +0100

    Improved bundle file for javascript

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -2,12 +2,14 @@ import axios from 'axios'
 import showAlert from './alert'
 const bookBtn = document.querySelector('#book-tour')
 
+console.log(Stripe)
+
 const stripe = Stripe
   ? Stripe(
       'pk_test_51IJhl0F5NUoFgs81W0nar8kX8M8rK42kkczl0W75HH9OGAgDBf8rNAFK8PToVzcLL1YlJ14kK1yQVofdACakEFJj00rVfTUcDw'
     )
   : ''
- 
+
 export const bookTour = async (tourId) => {
   try {
     const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`)
